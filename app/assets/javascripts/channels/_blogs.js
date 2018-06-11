d4d8a2ce0d67625179409cba3ffba3c178f3439a
AMKlein2018 <adamklein0519@gmail.com>
2018-06-10 23:04:52 -0400

    Rough Final without JS

---
+++ app/assets/javascripts/channels/_blogs.js
@@ -0,0 +1,13 @@
+var blogbutton = document.getElementById("blogbutton")
+var commentbutton = document.getElementById("commentbutton")
+
+blogbutton.addEventListener("click, function"() {
+
+
+});
+
+
+commentbutton.addEventListener("click, function"() {
+
+
+});
