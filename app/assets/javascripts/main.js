01004282f4c7443c8e063ef4749ee1aac1eaa4a2
AMKlein2018 <adamklein0519@gmail.com>
2018-06-12 15:23:27 -0400

    oggi is committing

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,17 @@
-var currentdate = new Date(); 
-var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
+var date = new Date();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var seconds = date.getSeconds();
+var clock = document.getElementById('clock');
+clock.textContent = hours ":" + minutes ":" + seconds;
+
+var myVar = setInterval(function(){
+		var date = new Date();
+		var hours = date.getHours();
+		var minutes = date.getMinutes();
+		var seconds = date.getSeconds();
+		clock.textContent = hours ":" + minutes ":" + seconds;
+
+
+
+}, 1000);
