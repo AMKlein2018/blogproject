b3c689402acd9f57e50dfc035e9886fbaed75496
AMKlein2018 <adamklein0519@gmail.com>
2018-06-12 15:25:07 -0400

    second commit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,17 @@
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-var clock = document.getElementById('clock');
-clock.textContent = hours ":" + minutes ":" + seconds;
+// var date = new Date();
+// var hours = date.getHours();
+// var minutes = date.getMinutes();
+// var seconds = date.getSeconds();
+// var clock = document.getElementById('clock');
+// clock.textContent = hours ":" + minutes ":" + seconds;
 
-var myVar = setInterval(function(){
-		var date = new Date();
-		var hours = date.getHours();
-		var minutes = date.getMinutes();
-		var seconds = date.getSeconds();
-		clock.textContent = hours ":" + minutes ":" + seconds;
+// var myVar = setInterval(function(){
+// 		var date = new Date();
+// 		var hours = date.getHours();
+// 		var minutes = date.getMinutes();
+// 		var seconds = date.getSeconds();
+// 		clock.textContent = hours ":" + minutes ":" + seconds;
 
 
 
-}, 1000);
+// }, 1000);
